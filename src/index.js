29b031d66a722d0943e709755272d6d8a9cf4cbb
Kris S <kristina19960712@gmail.com>
2022-08-11 21:39:40 +0300

    feat: first try solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = 
+function check(str, bracketsConfig) {
+  let arrValueBrackets = [];
+  let sum = 0;
+  let indexElement = -1;
+
+  bracketsConfig.forEach((element, index) => {
+    arrValueBrackets.push([10**index, (-1) * 10**index]);
+  }); 
+
+  str.split('').forEach((element)=>{
+    bracketsConfig.forEach((item, index)=>{ 
+      if (sum >= 0){
+        indexElement = item.indexOf(element);
+        sum += indexElement >=0 ? arrValueBrackets[index][indexElement ] : 0;
+      }
+      }
+    );
+  }
+  );
+  return sum === 0;
 }
+//const config1 =  [['(', ')'], ['|', '|']];
+//console.log(check('[(])', config1));
